46d70fe1864a0edb43af2a8cb8011b91cf2a1e74
sogo <sogonishi@gmail.com>
2024-03-02 23:01:09 +0900

    merge

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
-from flask import Flask, Response
-import yolo_app
+from flask import Flask, Response, render_template
+from yolo_app import generate_frames
 
 app = Flask(__name__)
 
-def generate():
-    # 動画ファイルを開く
-    with open('output.avi', 'rb') as video_file:
-        while True:
-            # 動画のバイナリデータを読み込んでストリームに書き込む
-            data = video_file.read(1024)
-            if not data:
-                break
-            yield data
-
-@app.route('/navi-ken-video')
+@app.route('/')
 def index():
-    # 動画ストリームをレスポンスとして返す
-    return Response(generate(), mimetype='video/x-msvideo')
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', debug=True)
+
--- yolo_app.py
+++ yolo_app.py
@@ -2,52 +2,42 @@ import cv2
 from ultralytics import YOLO
 
 # カメラのキャプチャを開始
-cap = cv2.VideoCapture(0)
-#cap = cv2.VideoCapture('data/sample2.MOV')
-
-# 動画ストリームの設定
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('data/sample2.MOV')
 
 # モデルのロード
 model = YOLO("yolov8n-seg.pt")
 
 # 面積検出フラグ
+global area_flg  # area_flg をグローバル変数として宣言
 area_flg = False
 
-while True:
-    # カメラから画像を読み込み
-    ret, frame = cap.read()
+def generate_frames():
+    global area_flg
+    while True:
+        # カメラから画像を読み込み
+        ret, frame = cap.read()
 
-    if area_flg:
-        cv2.putText(frame, 'WARNING!', (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 0, 255), 15)
-    else:
-        cv2.putText(frame, 'SAFE', (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 255, 0), 15)
-    
-    area_flg = False
-    
-    # 予測の実行
-    results = model.predict(source=frame, show=True, line_width=1) 
- 
-    # 予測結果の取得
-    for result in results:
-        for id, xywhn in zip(result.boxes.cls, result.boxes.xywhn):
-            area = xywhn[2] * xywhn[3]
-            print(id, area)
-            
-            if (area > 0.5) and (id == 0):
-                area_flg = True
+        if area_flg:
+            cv2.putText(frame, 'WARNING!', (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 0, 255), 15)
+        else:
+            cv2.putText(frame, 'SAFE', (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 255, 0), 15)
+        
+        area_flg = False
+        
+        # 予測の実行
+        results = model.predict(source=frame, show=True, line_width=1) 
 
-    # フレームを動画ストリームに書き込む
-    out.write(frame)
+        # 予測結果の取得
+        for result in results:
+            for id, xywhn in zip(result.boxes.cls, result.boxes.xywhn):
+                area = xywhn[2] * xywhn[3]
+                print(id, area)
+                
+                if (area > 0.5) and (id == 0):
+                    area_flg = True
 
-    # キー入力を待つ
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# キャプチャと動画ストリームを解放する
-cap.release()
-out.release()
-
-# ウィンドウをすべて閉じる
-cv2.destroyAllWindows()
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
